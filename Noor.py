529053c1384416e51d58d95299d1120aad7a5bc1
Noor <139367901+Noor-zadi@users.noreply.github.com>
2023-07-13 22:08:08 +0500

    Update Noor.py

--- Noor.py
+++ Noor.py
@@ -1,10 +1,10 @@
 import os,platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-  from Noor import Main
-	Main()
+    from Noor import Main
+    Main()
 elif bit == '32bit':
-	from Noor32 import Main
-  Main()
+    from Noor32 import Main
+    Main()
 else:
-	print('\n soory your device not support this tool');exit()
+    print('\n soory your device not support this tool');exit()
